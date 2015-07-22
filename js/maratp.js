94f96f27333e76cb4b2691b099a787d615990cf9
Bertrand JAN <bertrand.jan@alterway.fr>
2015-07-22 18:13:28 +0200

    picto help

--- js/maratp.js
+++ js/maratp.js
@@ -65,9 +65,39 @@
 
 	jQuery(document).click(function (event) {
 		// Clic extérieur à la navigation : fermeture du sous-menu affiché.
-		if (!(jQuery.contains(document.getElementById('navigation'), event.target))) {
-			jQuery('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
+		if(document.getElementById('navigation') !== null)
+		{
+			if(!(jQuery.contains(document.getElementById('navigation'), event.target))) {
+				jQuery('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
+			}
 		}
+	});
+
+// récupération de la couleur de fond du premier ancêtre non-transparent
+	function getAncestorBackgroundColor(node)
+	{
+		var bgc = node.css('background-color');
+		if(bgc === 'transparent')
+		{
+			return getAncestorBackgroundColor(node.parent());
+		}
+		else
+		{
+			return bgc;
+		}
+	}
+
+	// Titres avec filets
+
+	jQuery('.rich-display').each(function()
+	{
+		var current = jQuery(this);
+		var wrapText = jQuery('<span/>');
+
+		var bgColor = getAncestorBackgroundColor(current);
+		wrapText.css({'background-color': bgColor});
+
+		current.wrapInner(wrapText);
 	});
 
 // Carrousel.

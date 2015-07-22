7c1bd03f754531f49a90da564732dcc9c660cbf1
Bertrand JAN <bertrand.jan@alterway.fr>
2015-07-22 16:21:31 +0200

    design responsive

--- js/maratp.js
+++ js/maratp.js
@@ -3,114 +3,114 @@
 	//jQuery.noConflict();
 
 	// Gestion de l'affichage et du masquage des liens d'évitements.
-	$('#skiplinks').attr('class', 'hide');
-	$('#skiplinks a').each(function () {
-		$(this).blur(function () {
-			$('#skiplinks').attr('class', 'hide');
+	jQuery('#skiplinks').attr('class', 'hide');
+	jQuery('#skiplinks a').each(function () {
+		jQuery(this).blur(function () {
+			jQuery('#skiplinks').attr('class', 'hide');
 		});
-		$(this).focus(function () {
-			$('#skiplinks').attr('class', 'show');
+		jQuery(this).focus(function () {
+			jQuery('#skiplinks').attr('class', 'show');
 		});
 	});
 
 // Login.
 
-	$('header[role="banner"] > div:first-child > div > ul li:last-child a').mouseover(function () {
-		$(this).find('img').attr('src', 'img/maRATP-connexion-survol@2x.png');
+	jQuery('header[role="banner"] > div:first-child > div > ul li:last-child a').mouseover(function () {
+		jQuery(this).find('img').attr('src', 'img/maRATP-connexion-survol@2x.png');
 	});
 
-	$('header[role="banner"] > div:first-child > div > ul li:last-child a').focus(function () {
-		$(this).mouseover();
+	jQuery('header[role="banner"] > div:first-child > div > ul li:last-child a').focus(function () {
+		jQuery(this).mouseover();
 	});
 
-	$('header[role="banner"] > div:first-child > div > ul li:last-child a').mouseout(function () {
-		$(this).find('img').attr('src', 'img/maRATP-connexion@2x.png');
+	jQuery('header[role="banner"] > div:first-child > div > ul li:last-child a').mouseout(function () {
+		jQuery(this).find('img').attr('src', 'img/maRATP-connexion@2x.png');
 	});
 
-	$('header[role="banner"] > div:first-child > div > ul li:last-child a').blur(function () {
-		$(this).mouseout();
+	jQuery('header[role="banner"] > div:first-child > div > ul li:last-child a').blur(function () {
+		jQuery(this).mouseout();
 	});
 
 // Navigation.
 
-	$('#navigation span.button').each(function () {
+	jQuery('#navigation span.button').each(function () {
 		// Gestion des boutons permettant d'ouvrir les sous-menus.
-		$(this).attr('aria-label', $(this).text() + ', afficher le sous-menu ci-après');
-		$(this).attr('role', 'button');
-		$(this).attr('tabindex', '0');
+		jQuery(this).attr('aria-label', jQuery(this).text() + ', afficher le sous-menu ci-après');
+		jQuery(this).attr('role', 'button');
+		jQuery(this).attr('tabindex', '0');
 		// Evénements des boutons.
-		$(this).click(function () {
-			if ($(this).next().is('div[hidden]')) {
+		jQuery(this).click(function () {
+			if (jQuery(this).next().is('div[hidden]')) {
 				// Masquage du sous-menu précédemment affiché.
-				$('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
-				$(this).next().removeAttr('hidden');
-				$(this).attr('aria-label', $(this).text() + ', masquer le sous-menu');
+				jQuery('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
+				jQuery(this).next().removeAttr('hidden');
+				jQuery(this).attr('aria-label', jQuery(this).text() + ', masquer le sous-menu');
 			}
 			else {
-				$(this).next().attr('hidden', 'hidden');
-				$(this).attr('aria-label', $(this).text() + ', afficher le sous-menu ci-après');
+				jQuery(this).next().attr('hidden', 'hidden');
+				jQuery(this).attr('aria-label', jQuery(this).text() + ', afficher le sous-menu ci-après');
 			}
-			$(this).toggleClass('expanded');
+			jQuery(this).toggleClass('expanded');
 		});
-		$(this).keydown(function (event) { // keypress
+		jQuery(this).keydown(function (event) { // keypress
 			// Gestion des touches Entrée (13) et Espace (32).
 			if (event.which == 13 || event.which == 32) {
-				$(this).click();
+				jQuery(this).click();
 				event.preventDefault();
 			}
 		});
 		// Sous-menus masqués par défaut.
-		$(this).next().attr('hidden', 'hidden');
+		jQuery(this).next().attr('hidden', 'hidden');
 	});
 
-	$(document).click(function (event) {
+	jQuery(document).click(function (event) {
 		// Clic extérieur à la navigation : fermeture du sous-menu affiché.
-		if (!($.contains(document.getElementById('navigation'), event.target))) {
-			$('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
+		if (!(jQuery.contains(document.getElementById('navigation'), event.target))) {
+			jQuery('#navigation span[role="button"] + div').not('div[hidden]').prev().click();
 		}
 	});
 
 // Carrousel.
 
-	if ($('#carrousel').length) {
-		$('#carrousel').attr('class', 'carrousel-js');
-		$('#carrousel > div').attr('class', 'carrousel-js-1');
+	if (jQuery('#carrousel').length) {
+		jQuery('#carrousel').attr('class', 'carrousel-js');
+		jQuery('#carrousel > div').attr('class', 'carrousel-js-1');
 		// Système d'onglets.
-		var tablist = $('<ul role="tablist"></ul>');
-		$('#carrousel h3').each(function (index) {
+		var tablist = jQuery('<ul role="tablist"></ul>');
+		jQuery('#carrousel h3').each(function (index) {
 			// Onglet.
-			var tab = $('<li id="tab-' + (index + 1) + '" role="tab" aria-controls="tabpanel-' + (index + 1) + '" aria-selected="' + (index > 0 ? 'false' : 'true') + '" tabindex="' + (index > 0 ? '-1' : '0') + '"><img src="img/carrousel-item' + (index == 0 ? 'courant' : '') + '@2x.png" alt="' + $(this).text() + '" title="' + $(this).text() + '" /></li>');
+			var tab = jQuery('<li id="tab-' + (index + 1) + '" role="tab" aria-controls="tabpanel-' + (index + 1) + '" aria-selected="' + (index > 0 ? 'false' : 'true') + '" tabindex="' + (index > 0 ? '-1' : '0') + '"><img src="img/carrousel-item' + (index == 0 ? 'courant' : '') + '@2x.png" alt="' + jQuery(this).text() + '" title="' + jQuery(this).text() + '" /></li>');
 			tab.click(function () {
-				if ($(this).attr('aria-selected') == 'false') {
+				if (jQuery(this).attr('aria-selected') == 'false') {
 					// Onglet actuellement sélectionné.
-					var selected = $(this).parent().find('li[aria-selected="true"]');
+					var selected = jQuery(this).parent().find('li[aria-selected="true"]');
 					selected.attr('aria-selected', 'false').attr('tabindex', '-1');
 					selected.find('img').attr('src', 'img/carrousel-item@2x.png');
-					$('#' + selected.attr('aria-controls')).attr('aria-hidden', 'true');
+					jQuery('#' + selected.attr('aria-controls')).attr('aria-hidden', 'true');
 					// Nouvel onglet sélectionné.
-					$(this).attr('aria-selected', 'true').attr('tabindex', '0');
-					$(this).find('img').attr('src', 'img/carrousel-itemcourant@2x.png');
-					$('#' + $(this).attr('aria-controls')).attr('aria-hidden', 'false');
-					$('#carrousel > div').attr('class', 'carrousel-js-' + ($(this).parent().find('li').index($(this)) + 1));
+					jQuery(this).attr('aria-selected', 'true').attr('tabindex', '0');
+					jQuery(this).find('img').attr('src', 'img/carrousel-itemcourant@2x.png');
+					jQuery('#' + jQuery(this).attr('aria-controls')).attr('aria-hidden', 'false');
+					jQuery('#carrousel > div').attr('class', 'carrousel-js-' + (jQuery(this).parent().find('li').index(jQuery(this)) + 1));
 				}
 			});
 			// Navigation au clavier au sein des onglets.
 			tab.keydown(function (event) {
 				if (event.which == 37 || event.which == 38) {
 					// Onglet précédent (flèches gauche et haut).
-					var prev = $(this).prev().length ? $(this).prev() : $(this).parent().find('li').last();
+					var prev = jQuery(this).prev().length ? jQuery(this).prev() : jQuery(this).parent().find('li').last();
 					prev.focus().click();
 					event.preventDefault();
 				}
 				else if (event.which == 39 || event.which == 40) {
 					// Onglet suivant (flèches droite et bas).
-					var next = $(this).next().length ? $(this).next() : $(this).parent().find('li').first();
+					var next = jQuery(this).next().length ? jQuery(this).next() : jQuery(this).parent().find('li').first();
 					next.focus().click();
 					event.preventDefault();
 				}
 			});
 			tablist.append(tab);
-			$('#carrousel article:nth-child('+ (index + 1) +')').attr('id', 'tabpanel-' + (index + 1)).attr('role', 'tabpanel').attr('aria-hidden', (index > 0 ? 'true' : 'false')).attr('aria-labelledby', 'tab-' + (index + 1));
+			jQuery('#carrousel article:nth-child('+ (index + 1) +')').attr('id', 'tabpanel-' + (index + 1)).attr('role', 'tabpanel').attr('aria-hidden', (index > 0 ? 'true' : 'false')).attr('aria-labelledby', 'tab-' + (index + 1));
 		});
 		tablist.insertBefore('#carrousel > div');
 	}

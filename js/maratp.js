3d05f5397b6fc298a68af55b9c8a8a2270a7d2e6
morgane.mounier <morgane.mounier@alterway.fr>
2015-07-28 16:58:42 +0200

    corrections font

--- js/maratp.js
+++ js/maratp.js
@@ -33,3 +33,47 @@ jQuery(document).ready(function(){
   }
 
 });
+
+// Mini header.
+  
+   // A déporter dans un fichier commun pour gérer le responsive en contexte script.
+	var dashboardviewportwidth = 480;
+	var dashboardoldviewportwidth = false;
+	
+	function manageResponsiveDashboard() {
+		if (jQuery(window).width() != dashboardoldviewportwidth) {
+			if (jQuery(window).width() <= dashboardviewportwidth) {
+				var burger = jQuery('.mini-header span.button');
+			  burger.attr('role', 'button').attr('tabindex', '0').attr('aria-label', jQuery('.mini-header h1').text() + ', afficher le menu ci-après');
+			  var menu = jQuery('.menu-dashboard');
+			  menu.attr('hidden', 'hidden');
+			  menu.addClass('menu-dashboard-responsive');
+			  burger.click(function () {
+			  	var menu = jQuery('.menu-dashboard');
+			  	if (menu.is('[hidden]')) {
+			  		jQuery(this).attr('aria-label', jQuery('.mini-header h1').text() + ', masquer le menu');
+			  		menu.removeAttr('hidden');
+			  	}
+			  	else {
+			  		jQuery(this).attr('aria-label', jQuery('.mini-header h1').text() + ', afficher le menu ci-après');
+			  		menu.attr('hidden', 'hidden');
+			  	}
+			  }).keydown(function (event) {
+			  	if (event.which == 13 || event.which == 32) {
+			  		jQuery(this).click();
+			  	}
+			  });
+			}
+			else {
+				var burger = jQuery('.mini-header span.button');
+			  burger.removeAttr('role').removeAttr('tabindex').removeAttr('aria-label');
+			  var menu = jQuery('.menu-dashboard');
+			  menu.removeClass('menu-dashboard-responsive');
+			  menu.removeAttr('hidden');
+			  burger.off();
+			}	
+		}
+		dashboardoldviewportwidth = jQuery(window).width();
+	}
+	jQuery(window).ready(manageResponsiveDashboard);
+	jQuery(window).resize(manageResponsiveDashboard);

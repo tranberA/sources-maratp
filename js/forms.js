1e8ea7480d94fb6c23d0343e54b761f5ed9ab6fb
Bertrand JAN <bertrand.jan@alterway.fr>
2015-07-30 16:24:58 +0200

    navigation clavier form

--- js/forms.js
+++ js/forms.js
@@ -1,13 +1,120 @@
 if(window.jQuery !== undefined)
 {
+  function activatePanel(panel)
+  {
+    var nextFocus = panel.data('next-focus');
+
+    if(nextFocus !== undefined)
+    {
+      //jQuery(nextFocus).focus();
+    }
+
+    panel.attr('aria-hidden','false');
+    //panel.addClass('panel-active');
+    panel.find('[tabindex=-1]').attr('data-ti','true').removeAttr('tabindex');
+    panel.focus();
+    //.find('input[type=text]')
+  }
+
+  function inactivatePanel(panel,root)
+  {
+    // var previousFocus = panel.data('prev-focus');
+    //
+    // if(previousFocus !== undefined)
+    // {
+    //   jQuery(previousFocus).focus();
+    // }
+
+    if(root)
+    {
+      jQuery('#start-form').focus();
+    }
+
+
+    //panel.removeClass('panel-active');
+    panel.attr('aria-hidden','true');
+    panel.find('[data-ti=true]').data('ti','false').attr('tabindex','-1');
+    //panel.blur();
+  }
+
   jQuery(function()
   {
     // Navigation entre panneaux de formulaire Mes privilèges keyup
 
-    jQuery('#start-form').focus();
+    //jQuery('#start-form').focus();
+    //
+  	// var currentPanel = null;
+  	// jQuery('.panel-action').on('click',function(event)
+  	// {
+  	// 	var current = jQuery(this);
+  	// 	var target = current.data('target');
+    //
+  	// 	if(target !== undefined)
+  	// 	{
+  	// 		var targetPanel = jQuery('#'+target);
+    //
+  	// 		if(targetPanel.length > 0)
+  	// 		{
+    //       var canClose = true;
+    //
+    //       var closeData = current.data('close');
+    //
+    //       if(closeData !== undefined)
+    //       {
+    //         canClose = !!closeData;
+    //       }
+    //
+    //       if(canClose)
+    //       {
+    //         var close = targetPanel.find('.panel-close');
+    //
+    //         if(close.length == 0)
+    //         {
+    //           var closeBtn = jQuery('<button class="panel-close ico-close" tabindex="-1">Fermer</button>').on('click',function(event)
+    //             {
+    //               if(currentPanel !== null)
+    //       				{
+    //                 inactivatePanel(currentPanel);
+    //       				}
+    //
+    //               event.preventDefault();
+    //             });
+    //
+    //           targetPanel.append(closeBtn);
+    //         }
+    //       }
+    //
+  	// 			if(currentPanel !== null && !targetPanel.is(currentPanel) && currentPanel.hasClass('panel-active'))
+  	// 			{
+    //         inactivatePanel(currentPanel);
+  	// 			}
+    //
+    //       activatePanel(targetPanel);
+    //
+  	// 			currentPanel = targetPanel;
+  	// 		}
+  	// 	}
+    //
+    //   event.preventDefault();
+  	// });
 
-  	var currentPanel = null;
-  	jQuery('.panel-action').on('click keyup',function(event)
+    var formSlide = jQuery('#form-slide-block');
+    var form = jQuery('.form-slide',formSlide);
+    var itemCollection = form.find('.form-panel');
+    var formWidth = 0;
+    var blockWidth = formSlide.outerWidth();
+    itemCollection.each(function(i)
+    {
+      formWidth += blockWidth;
+      var currentBlock = jQuery(this);
+      currentBlock.data('left',blockWidth*i);
+      currentBlock.outerHeight(formSlide.outerHeight()).outerWidth(blockWidth);
+    });
+
+    form.width(formWidth);
+
+    var currentPanel = null;
+    jQuery('.panel-action').on('click',function(event)
   	{
   		var current = jQuery(this);
   		var target = current.data('target');
@@ -20,7 +127,7 @@ if(window.jQuery !== undefined)
   			{
           var canClose = true;
 
-          var closeData = current.data('close');
+          var closeData = targetPanel.data('close');
 
           if(closeData !== undefined)
           {
@@ -33,11 +140,11 @@ if(window.jQuery !== undefined)
 
             if(close.length == 0)
             {
-              var closeBtn = jQuery('<button class="panel-close ico-close">Fermer</button>').on('click keyup',function(event)
+              var closeBtn = jQuery('<button class="panel-close ico-close" tabindex="-1">Fermer</button>').on('click',function(event)
                 {
                   if(currentPanel !== null)
           				{
-          					currentPanel.removeClass('panel-active');
+                    inactivatePanel(currentPanel,true);
           				}
 
                   event.preventDefault();
@@ -46,24 +153,27 @@ if(window.jQuery !== undefined)
               targetPanel.append(closeBtn);
             }
           }
-
-          //targetPanel.focus();
-  				targetPanel.addClass('panel-active');
-
-// focus sur le lien panneau suivant ???
-          //targetPanel.find('.panel-next').focus();
-
-  				if(currentPanel !== null && !targetPanel.is(currentPanel) && currentPanel.hasClass('panel-active'))
+// && currentPanel.hasClass('panel-active')
+          if(currentPanel !== null && !targetPanel.is(currentPanel))
   				{
-  					currentPanel.removeClass('panel-active');
+            inactivatePanel(currentPanel,false);
   				}
 
-  				currentPanel = targetPanel;
-  			}
-  		}
+          activatePanel(targetPanel);
+
+          var panelLeft = parseInt(targetPanel.data('left'));
+          //console.log(panelLeft);
+
+          //form.css({'left': -panelLeft});
+
+        	currentPanel = targetPanel;
+        }
+      }
 
       event.preventDefault();
-  	});
+    });
+
+
 /*
     jQuery('#last-step').on('click',function(ev)
     {
